29bcbf0fa76d6c98791545be499af0462848a5fa
Haluzdarck <104384350+aaronestradabonilla777@users.noreply.github.com>
2022-05-10 10:52:08 -0700

    third commit  get point hola and adios

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app =express ();
 
-app.get('/', function (req, res){
+app.get('/Hola', function (req, res){
     res.send('Hola Mundo!');
 });
+
+app.get('/Adios', function (req, res){
+    res.send('Adios Mundo!');
+});
+
 
 app.listen(3000, function(){
     console.log('Aplicando ejemplo, escuchando el puerto 3000!');
