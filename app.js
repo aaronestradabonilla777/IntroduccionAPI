ac8858b599abb369f7da61feb83a7f4c8a0cb52d
Haluzdarck <104384350+aaronestradabonilla777@users.noreply.github.com>
2022-05-11 10:11:51 -0700

    commit server core

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app =express ();
 
 app.get('/Hola', function (req, res){
@@ -12,4 +12,30 @@ app.get('/Adios', function (req, res){
 
 app.listen(3000, function(){
     console.log('Aplicando ejemplo, escuchando el puerto 3000!');
+});*/ 
+//EJEMPLO DE  ENDPOINT
+
+//Requiere packages and set the port
+const express = require('express');
+const port = 3002;
+//para permitir manejo de POST Y PUT
+const bodyParser = require('body-parser');
+const routes= require('./routes/routes')
+const app= express();
+
+//usar node,js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(
+    {
+        extended: true,
+    }
+));
+
+routes(app);
+
+//iniciar el servidor
+const server =app.listen (port, (error)=>{
+    if(error) return console.log(`Error: ${error}`);
+
+    console.log(`El servidor escucha en el puerto ${server.address().port}`);
 });
